66e9eb477578bfec107bd521a747dd6139156cb8
Dusko <radovic_dusko@yahoo.com>
2021-03-19 10:24:10 +0100

    initial setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Django
+          Learn Nothing
         </a>
       </header>
     </div>
